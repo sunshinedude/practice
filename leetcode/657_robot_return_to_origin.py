528cfabc18f27eaacd86a78db1ee2b4ce64d5e6a
sunshinedude <sunshine.dude@yandex.ru>
2020-01-29 13:38:35 +0300

    Added 657 task with two solutions

---
+++ leetcode/657_robot_return_to_origin.py
@@ -0,0 +1,62 @@
+# Problem
+
+# There is a robot starting at position (0, 0), the origin, on a 2D plane.
+# Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+#
+# The move sequence is represented by a string, and the character moves[i] represents its ith move.
+# Valid moves are R (right), L (left), U (up), and D (down).
+# If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
+#
+# Note: The way that the robot is "facing" is irrelevant.
+# "R" will always make the robot move to the right once, "L" will always make it move left, etc.
+# Also, assume that the magnitude of the robot's movement is the same for each move.
+#
+# Example 1:
+#
+# Input: "UD"
+# Output: true
+# Explanation: The robot moves up once, and then down once.
+# All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
+#
+#
+# Example 2:
+#
+# Input: "LL"
+# Output: false
+# Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin.
+# We return false because it is not at the origin at the end of its moves.
+
+# Solution
+from leetcode.test import Test
+
+
+class Solution:
+	# Base solution
+	def judgeCircle(self, moves: str) -> bool:
+		moves_as_dict = {
+			'U': 0,
+			'D': 0,
+			'L': 0,
+			'R': 0
+		}
+		for c in moves:
+			moves_as_dict[c] += 1
+		return moves_as_dict['U'] == moves_as_dict['D'] and moves_as_dict['L'] == moves_as_dict['R']
+
+
+class Solution_1:
+	# Python solution
+	def judgeCircle(self, moves: str) -> bool:
+		return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
+
+
+# Tests
+
+cases = [
+	{"input": "UD", "output": True},
+	{"input": "LL", "output": False},
+]
+Test(Solution().judgeCircle, cases, True).test()
+Test(Solution_1().judgeCircle, cases, True).test()
+
+
