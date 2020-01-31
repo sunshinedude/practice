254b5bc5bd0c0d2c71d983ada0bd6bac577ba4b7
sunshinedude <sunshine.dude@yandex.ru>
2020-02-01 00:00:35 +0300

    Added TODO to 617 task

--- leetcode/617_merge_two_binary_trees.py
+++ leetcode/617_merge_two_binary_trees.py
@@ -49,3 +49,5 @@ class Solution:
         t1.left = self.mergeTrees(t1.left, t2.left)
         t1.right = self.mergeTrees(t1.right, t2.right)
         return t1
+
+# TODO: Added non recursion method.
