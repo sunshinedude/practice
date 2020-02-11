43fb1fc3e9f7952376c65ee2dee4d320f5854906
sunshinedude <sunshine.dude@yandex.ru>
2020-02-11 03:22:53 +0300

    Added 1047 task.

---
+++ leetcode/1047_remove_all_adjacent_duplicates_in_string.py
@@ -0,0 +1,41 @@
+# Problem
+
+# Given a string S of lowercase letters, a duplicate removal consists of
+# choosing two adjacent and equal letters, and removing them.
+#
+# We repeatedly make duplicate removals on S until we no longer can.
+#
+# Return the final string after all such duplicate removals have been made.  It is guaranteed the answer is unique.
+#
+#
+#
+# Example 1:
+#
+# Input: "abbaca"
+# Output: "ca"
+# Explanation:
+# For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal,
+# and this is the only possible move.  The result of this move is that the string is "aaca",
+# of which only "aa" is possible, so the final string is "ca".
+
+# Solution
+from leetcode.test import Test
+
+
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        ans = []
+        for i in S:
+            if ans and ans[-1] == i:
+                ans.pop()
+            else:
+                ans.append(i)
+        return "".join(ans)
+
+
+# Tests
+
+cases = [
+    {"input": "abbaca", "output": "ca"},
+]
+Test(Solution().removeDuplicates, cases, True).test()
